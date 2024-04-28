e03c62b48fe87fa1b0242c2dd9a76f28ca05664f
AlexHdz357 <A01571408@tec.mx>
2024-04-28 13:58:16 -0600

    FIREBASECOMPLETO

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+
+const app = express();
+
+app.get("/hello-world", (req, res) => {
+  return res.status(200).json({message: "Hello World!"});
+});
+
+app.use(require("./reportes.routes"));
+
+exports.app = functions.https.onRequest(app);
+
---
+++ functions/reportes.routes.js
@@ -0,0 +1,110 @@
+const {Router} = require("express");
+const admin = require("firebase-admin");
+const router = Router();
+
+module.exports = router;
+
+admin.initializeApp({
+  credential: admin.credential.cert("./permissions.json"),
+  databaseURL: "http://fb-api-1fbee.firebaseio.com",
+});
+
+const db = admin.firestore();
+
+router.post("/api/reportes", async (req, res) => {
+    try {
+      await db.collection("reportes")
+          .doc("/" + req.body.idReporte + "/")
+          .create({nombreProducto: req.body.nombreProducto,
+            idEmpleado: req.body.idEmpleado,
+            prioridad: req.body.prioridad,
+            descripcion: req.body.descripcion,
+            latitude: req.body.latitude,
+            longitude: req.body.longitude,
+            imagenTexto: req.body.imagenTexto,
+            aprobado: req.body.aprobado,
+            completado: req.body.completado,
+            fechaIngreso: new Date(),
+          });
+      return res.status(204).json();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  });
+  
+router.get("/api/reportes/:idReporte", async (req, res) => {
+    (async () => {
+      try {
+        const doc = db.collection("reportes").doc(req.params.idReporte);
+        const item = await doc.get();
+        const response = item.data();
+        return res.status(200).json(response);
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+      }
+    })();
+  });
+  
+router.get("/api/reportes", async (req, res) => {
+    try {
+      const query = db.collection("reportes");
+      const querySnapshot = await query.get();
+      const docs = querySnapshot.docs;
+      const response = docs.map((doc) => ({
+        idReporte: doc.idReporte,
+        nombreProducto: doc.data().nombreProducto,
+        idEmpleado: doc.data().idEmpleado,
+        prioridad: doc.data().prioridad,
+        descripcion: doc.data().descripcion,
+        latitude: doc.data().latitude,
+        longitude: doc.data().longitude,
+        imagenTexto: doc.data().imagenTexto,
+        aprobado: doc.data().aprobado,
+        completado: doc.data().completado,
+        fecha: doc.data().fecha,
+      }));
+      return res.status(200).json(response);
+    } catch (error) {
+      return res.status(500).json();
+    }
+  });
+  
+  
+router.put("/api/reportes/:idReporte", async (req, res) => {
+    const {idReporte} = req.params;
+    const {aprobado, completado} = req.body;
+    try {
+      const doc = db.collection("reportes").doc(idReporte);
+      const currentData = (await doc.get()).data() || {};
+      const reporteActualizado = {
+        ...currentData,
+        ...(aprobado !== undefined && {aprobado}),
+        ...(completado !== undefined && {completado}),
+      };
+      await doc.update(reporteActualizado);
+      return res.status(200).json({idReporte, ...reporteActualizado});
+    } catch (error) {
+      return res.status(500).json({error: error.toString()});
+    }
+  });
+
+  
+router.post("/api/empleados", async (req, res) => {
+  try {
+    await db.collection("empleados")
+        .doc("/" + req.body.idEmpleado + "/")
+        .create({
+          NombreCompleto: req.body.NombreCompleto,
+          Rol: req.body.Rol,
+          Puntos: req.body.Puntos,
+          fecha: new Date(),
+        });
+    return res.status(204).json();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
+
